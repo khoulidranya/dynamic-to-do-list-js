fa8a8dff7838649c22e23627c0a40ae3e7954fcf
khoulidranya <khoulidranya@gmail.com>
2024-10-13 16:25:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const addButton = document.getElementById("add-task-button");
+document.addEventListener("DOMContentLoaded", function () {
+    const addButton = document.getElementById("add-task-btn");
     const taskInput = document.getElementById("task-input");
     const taskList = document.getElementById("task-list");
     function addTask() {
@@ -12,18 +12,16 @@ document.addEventListener("DOMContentLoaded", function() {
         listItem.textContent = taskText;
         const removeButton = document.createElement("button");
         removeButton.textContent = "Remove";
-        removeButton.className = "remove-btn";
-        removeButton.onclick = function() {
+        removeButton.classList.add("remove-btn");
+        removeButton.addEventListener("click", function () {
             taskList.removeChild(listItem);
-        };
-
-        
+        });
         listItem.appendChild(removeButton);
         taskList.appendChild(listItem);
         taskInput.value = "";
     }
     addButton.addEventListener("click", addTask);
-    taskInput.addEventListener("keypress", function(event) {
+    taskInput.addEventListener("keypress", function (event) {
         if (event.key === "Enter") {
             addTask();
         }
